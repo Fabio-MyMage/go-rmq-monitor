b9a1b9b40f8ba5ff0f86cfd76aa13aeb76928930
Fabio <fabio@mymage.io>
2025-10-27 13:37:56 +0400

    Add pipeline to build and release on tags

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "go-rmq-monitor/cmd"
+import (
+	"fmt"
+	"os"
+
+	"go-rmq-monitor/cmd"
+)
+
+var (
+	version = "v0.0.1"
+	commit  = "none"
+	date    = "unknown"
+)
 
 func main() {
+	// Handle version command
+	if len(os.Args) > 1 && os.Args[1] == "version" {
+		fmt.Printf("go-rmq-monitor version %s\n", version)
+		fmt.Printf("commit: %s\n", commit)
+		fmt.Printf("built: %s\n", date)
+		os.Exit(0)
+	}
+
 	cmd.Execute()
 }
